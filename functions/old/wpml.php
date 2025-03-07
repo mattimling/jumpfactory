fdcd1f0c7bfe442f06b527e17d364354bace2843
MATT\matti <mattimling@gmail.com>
2025-03-07 10:13:05 +0100

    Cleanup

--- functions/old/wpml.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-// Get current language.
-function get_language_shortcode() {
-	return apply_filters( 'wpml_current_language', null );
-}
-add_shortcode( 'language', 'get_language_shortcode' );
