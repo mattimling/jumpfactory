cc30e43cf107db606faffdd62fbbc245138b6bed
MATT\matti <mattimling@gmail.com>
2025-04-02 14:11:32 +0200

    Headline update

--- components/acf-blocks/headline.php
+++ components/acf-blocks/headline.php
@@ -3,11 +3,11 @@
 $text = get_sub_field( 'text' );
 $headline_type = get_sub_field( 'headline_type' );
 
-if ( $text && ( $headline_type == 'H1' || $headline_type == 'H2' ) ) : ?>
+if ( $text && ( $headline_type == 'H1' || $headline_type == 'H2' || $headline_type == 'H3' ) ) : ?>
 
 	<div class="px-8 lg:px-16 js-element-blurin">
 
-		<<?= strtolower( $headline_type ); ?> class="text-h1 text-red will-change-transform break-words">
+		<<?= strtolower( $headline_type ); ?> class="<?= ( $headline_type == 'H1' || $headline_type == 'H2' ) ? 'text-h1' : 'text-h3'; ?> text-red will-change-transform break-words">
 
 			<?= $text; ?>
 
