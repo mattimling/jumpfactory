e886979cdab89cef22b629e6421354fcc51f02bf
MATT\matti <mattimling@gmail.com>
2025-05-10 18:23:43 +0200

    Blocks update

--- pages/blocks.php
+++ pages/blocks.php
@@ -7,24 +7,16 @@
 <?php get_template_part( 'components/header/header' ); ?>
 
 <?php if ( class_exists( 'ACF' ) ) : ?>
-
 	<?php
-
 	$id = get_the_ID();
 	$hero_without_image = get_field( 'hero_without_image', $id );
-
 	?>
 
-	<div class="flex flex-col gap-y-16 lg:gap-y-32 mb-16 lg:mb-32 <?= $hero_without_image ? 'pt-24 lg:pt-40' : ''; ?>">
-
+	<div class="flex flex-col gap-y-16 lg:gap-y-32 Xmb-16 Xlg:mb-32 <?= $hero_without_image ? 'pt-24 lg:pt-40' : ''; ?>">
 		<?php get_template_part( 'components/acf-blocks/index' ); ?>
-
 	</div>
-
 <?php else : ?>
-
 	Theme needs ACF plugin to work properly
-
 <?php endif; ?>
 
 <?php get_template_part( 'components/footer/footer' ); ?>
