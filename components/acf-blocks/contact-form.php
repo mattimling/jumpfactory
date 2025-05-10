490dbec73a9cd6889717f98c7d5fbc94d9e845be
MATT\matti <mattimling@gmail.com>
2025-05-10 18:22:52 +0200

    Contact form embed code

--- components/acf-blocks/contact-form.php
+++ components/acf-blocks/contact-form.php
@@ -1,23 +1,16 @@
 <?php
-
+$form_embed = get_sub_field( 'form_embed' );
 $text = get_sub_field( 'text' );
-
 ?>
 
 <div class="px-8 lg:px-16 grid grid-cols-12 gap-y-16 lg:gap-x-16">
-
-	<div class="col-span-12 lg:col-span-6 xl:col-span-5">
-
-		Contact Form
-
+	<div class="col-span-12 lg:col-span-6 xl:col-span-5 contact-form js-element-blurin">
+		<?= $form_embed; ?>
 	</div>
 
-	<div class="col-span-12 lg:col-span-6 xl:col-span-5 xl:col-start-8">
-
+	<div class="col-span-12 lg:col-span-6 xl:col-span-5 xl:col-start-8 js-element-blurin">
 		<div class="text-h5 text-blue">
 			<?= $text; ?>
 		</div>
-
 	</div>
-
 </div>
